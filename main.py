70d81c33069a9d27d5c9e3d00e5b240c783b64bc
wirlyswirlycurlysandoval <sandoval.dg@obmontessori.edu.ph>
2026-09-19 21:36:01 +0800

    styling

--- main.py
+++ main.py
@@ -2,24 +2,16 @@ from pyscript import display
 from js import document
 
 def create_order(e):
-    document.getElementbyId('order').HTML = ""
+    document.getElementById("order").innerHTML = ""
+    drink1 = document.getElementById("drink1")
+    drink2 = document.getElementById("drink2")
+    drink3 = document.getElementById("drink3")
+    drink4 = document.getElementById("drink4")
+    drink5 = document.getElementById("drink5")
+    large = document.getElementById("large")
 
-    drink1 = document.getElementbyId('drink1')
-    drink2 = document.getElementbyId('drink2')
-    drink3 = document.getElementbyId('drink3')
-    drink4 = document.getElementbyId('drink4')
-    drink5 = document.getElementbyId('drink5')
-
-
-    subtotal = float(drink1.value) * drink1.checked + float(drink2.value) * drink2.checked + float(drink3.value) * drink3.checked + float(drink4.value) * drink4.checked + float(drink5.value) * drink5.checked
+    subtotal = float(drink1.value) * drink1.checked + float(drink2.value) * drink2.checked + float(drink3.value) * drink3.checked + float(drink4.value) * drink4.checked + float(drink5.value) * drink5.checked + float(large.value) * large.checked
 
     vat = subtotal * 0.12
-    amount_due = subtotal + vat
-
-    display(
-        "Dani's Boba Shop!",
-        f"Subtotal: ₱{subtotal:.2f}",
-        f"VAT: ₱{vat:.2f}",
-        f"Amount Due: ₱{amount_due.2f}",
-        target="reciept"
-    )
+    grand_total = subtotal + vat
+    display(f"Subtotal: {subtotal}", f"VAT: {vat}", f"Grand Total: {grand_total}", target="order")
